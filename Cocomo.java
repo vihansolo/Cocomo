c19a828acc6c66c6af7155d6c230e2b888bb9e90
Vihang Garud <garudvihang0@gmail.com>
2018-02-06 09:32:44 +0530

    added two classes - basic and intermediate

--- Cocomo.java
+++ Cocomo.java
@@ -2,10 +2,20 @@ import java.util.*;
 
 class Cocomo {
 
+	static void Basic() {
+
+		
+	}
+
+	static void Intermediate() {
+
+
+	}
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 
 
 	}
-}	
+}
