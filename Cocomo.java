c28af0bf8eb8d86484290cf27a60023bfa60d5b8
Vihang Garud <garudvihang0@gmail.com>
2018-02-06 09:42:41 +0530

    Modified class Basic

--- Cocomo.java
+++ Cocomo.java
@@ -1,16 +1,36 @@
 import java.util.*;
 
+class Basic() {
+
+	double[] a = new double[];
+	double[] b = new double[];
+	double[] c = new double[];
+	double[] d = new double[];
+
+	a[0] = 2.4;
+	a[1] = 3;
+	a[2] = 3.6;
+
+	b[0] = 1.05;
+	b[1] = 1.12;
+	b[2] = 1.20;
+
+	c[0] = 2.5;
+	c[1] = 2.5;
+	c[2] = 2.5;
+
+	d[0] = 0.38;
+	d[1] = 0.36;
+	d[2] = 0.32;
+
+}
+
+class Intermediate() {
+
+}
+
+
 class Cocomo {
-
-	static void Basic() {
-
-		
-	}
-
-	static void Intermediate() {
-
-
-	}
 
 	public static void main(String[] args) {
 
